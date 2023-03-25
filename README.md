fdf2ae5fa23ac1a941e8875f8072b2f4c289b469
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2023-03-26 00:30:26 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# vue-3-composition-api-show-element-position
-Created with CodeSandbox
+# How to show the position of an element in Vue 3 Composition API | Example
+
+Learn how to show the position of an element in Vue 3 Composition API with working demo example [Learn more](https://www.nightprogrammer.com/vue-js/show-the-position-of-an-element-in-vue-3-composition-api--example/)
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
+
+### Lints and fixes files
+
+```
+yarn lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+
+const targetElement = ref(null);
+const positionInformation = ref(null);
+
+const showPosition = () => {
+  const {
+    top,
+    right,
+    bottom,
+    left,
+  } = targetElement.value.getBoundingClientRect();
+  positionInformation.value = `Position: Top: ${top}, Right: ${right}, Bottom: ${bottom}, Left: ${left}`;
+};
+</script>
+
+<template>
+  <div>
+    <div ref="targetElement" class="show-position" @click="showPosition">
+      Show position
+    </div>
+    <div>
+      {{ positionInformation }}
+    </div>
+  </div>
+</template>
+
+<style>
+.show-position {
+  background-color: green;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
